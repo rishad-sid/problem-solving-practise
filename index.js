4e15a0cd6576006448bdb248771245435d191afa
rishad-sid <rishad.sid@gmail.com>
2022-10-26 03:42:08 +0600

    Palindrime string solution

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const result = require("./string reversal/index");
+const palindromeResult = require("./palindrome/index");
 const app = express();
 
 
 console.log(`Successfully solved the reverse string problem and the solution is "${result}"`);
+console.log(`Successfully solved the palindrome string problem and the solution is "${palindromeResult}"`);
 
 const port = 3000;
 
