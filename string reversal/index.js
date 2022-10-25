230df41f5bc95ef8e524c89bcc609e3a9f5b79e6
rishad-sid <rishad.sid@gmail.com>
2022-10-26 02:04:51 +0600

    three total solutions for reverse string problem

--- string reversal/index.js
+++ string reversal/index.js
@@ -6,11 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+
+
+//Solution #1
+
+// function reverse(str) {
+//     return str
+//         .split('')
+//         .reverse()
+//         .join('')
+// }
+
+
+
+//Solution #2
+
+
+// function reverse(str) {
+//     let reversed = '';
+//     for (let characters of str) {
+//         reversed = characters + reversed;
+//     }
+
+//     return reversed;
+// }
+
+
+
+//Solution #3
+
+
 function reverse(str) {
-    return str
-        .split('')
-        .reverse()
-        .join('')
+    return str.split('').reduce((reversed, characters) => characters + reversed, '');
 }
 
 const result = reverse("Rishad Siddiqy");
