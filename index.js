5ea60b3ec43f661c513ca7b829657c22e6dad6ee
rishad-sid <rishad.sid@gmail.com>
2022-10-26 22:22:31 +0600

    Solution of max character in a string problem

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const result = require("./string reversal/index");
 const palindromeResult = require("./palindrome/index");
 const reverseIntResult = require("./reverse integer/index");
+const maxCharResult = require("./max character/index");
 const app = express();
 
 
 console.log(`Successfully solved the reverse string problem and the solution is "${result}"`);
 console.log(`Successfully solved the palindrome string problem and the solution is "${palindromeResult}"`);
 console.log(`Successfully solved the reverse integer problem and the solution is "${reverseIntResult}"`);
+console.log(`Successfully solved the Max Character in a string problem and the solution is "${maxCharResult}"`);
 
 const port = 3000;
 
