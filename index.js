dac8ebb9319e824a40cac71c2aa296405cc920bf
rishad-sid <rishad.sid@gmail.com>
2022-10-26 21:20:13 +0600

    reverse integer solution

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const result = require("./string reversal/index");
 const palindromeResult = require("./palindrome/index");
+const reverseIntResult = require("./reverse integer/index");
 const app = express();
 
 
 console.log(`Successfully solved the reverse string problem and the solution is "${result}"`);
 console.log(`Successfully solved the palindrome string problem and the solution is "${palindromeResult}"`);
+console.log(`Successfully solved the reverse integer problem and the solution is "${reverseIntResult}"`);
 
 const port = 3000;
 
