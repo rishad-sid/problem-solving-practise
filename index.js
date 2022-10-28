827ac019550e8c4211e4c06285a8c9999feb56db
rishad-sid <rishad.sid@gmail.com>
2022-10-28 19:08:12 +0600

    Solution of anagram problem

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const palindromeResult = require("./palindrome/index");
 const reverseIntResult = require("./reverse integer/index");
 const maxCharResult = require("./max character/index");
 const chunkResult = require("./array chunk/index");
+const anagramResult = require("./anagram/index");
 const app = express();
 
 
@@ -12,7 +13,7 @@ console.log(`Successfully solved the palindrome string problem and the solution 
 console.log(`Successfully solved the reverse integer problem and the solution is "${reverseIntResult}"`);
 console.log(`Successfully solved the Max Character in a string problem and the solution is "${maxCharResult}"`);
 console.log("Successfully solved the array chunk problem and the solution is" + " ", chunkResult);
-
+console.log(`Successfully solved the anagram problem and the solution is "${anagramResult}"`);
 
 const port = 3000;
 
